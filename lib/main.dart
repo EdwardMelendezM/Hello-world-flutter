9ab9367716d95d2ba1486a3b7f5120a2cf5f4dd3
EdwardMelendezM <192666@unsaac.edu.pe>
2023-05-27 13:22:35 -0500

    create proyect

---
+++ lib/main.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+void main(){
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget{
+  @override
+  Widget build(Object context) {
+    // TODO: implement build
+    return MaterialApp(
+      home: Center(child: Text('Hola mudno'),),
+    );
+  }
+
+}
